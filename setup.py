3206bdc5762be1e780ccd1e698614e7c4dad15c7
Miao Xi <miao.xi@mail.utoronto.ca>
2022-10-15 03:38:56 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/19PlIU96bqEzFokjzdaxMslQlV6DZ0jZ_
 """
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
    name='timeSeriesHelper',
